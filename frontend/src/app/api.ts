3737b6755c3592209336bc52a8af2abb5c357630
Joe Alongi <contact@joealongi.dev>
2026-01-04 04:52:00 -0500

    Add Angular environment files for dynamic API URLs

--- frontend/src/app/api.ts
+++ frontend/src/app/api.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Api {
-  private baseUrl = 'https://api.airwave.fitness/api/';
+  private baseUrl = environment.apiUrl + '/api/';
 
   constructor(private http: HttpClient) {}
 
